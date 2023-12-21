569ec92b387859980a9365170b899485f99f83ff
Oladipo Kayode <32282156+oluwakayode-a@users.noreply.github.com>
2023-12-20 21:21:15 -0500

    Tried localhost

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -27,7 +27,7 @@ DATABASES = {
         'NAME': os.getenv('DATABASE_NAME'),
         'USER' : os.getenv('DATABASE_USER'),
         'PASSWORD' : os.getenv('DATABASE_PASSWORD'),
-        'HOST' : 'db',
+        'HOST' : 'localhost',
         'PORT' : '5432'
     }
 }
